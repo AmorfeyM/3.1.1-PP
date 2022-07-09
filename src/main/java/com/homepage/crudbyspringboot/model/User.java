8c50a58588e14f7138c9caf2a687935f98fb9f3a
Sergey Popov <popovsv1987@gmail.com>
2022-07-09 10:10:35 +0300

    bugfix

--- src/main/java/com/homepage/crudbyspringboot/model/User.java
+++ src/main/java/com/homepage/crudbyspringboot/model/User.java
@@ -1,10 +1,6 @@
 package com.homepage.crudbyspringboot.model;
-
-import lombok.Data;
-
 import javax.persistence.*;
 
-@Data
 @Entity
 @Table(name = "users")
 public class User {
@@ -22,4 +18,54 @@ public class User {
 
    @Column(name = "email")
    private String email;
+
+   public User() {
+   }
+
+   public User(String firstName, String lastName, String email) {
+      this.firstName = firstName;
+      this.lastName = lastName;
+      this.email = email;
+   }
+
+   public Long getId() {
+      return id;
+   }
+
+   public String getFirstName() {
+      return firstName;
+   }
+
+   public String getLastName() {
+      return lastName;
+   }
+
+   public String getEmail() {
+      return email;
+   }
+
+   public void setId(Long id) {
+      this.id = id;
+   }
+
+   public void setFirstName(String firstName) {
+      this.firstName = firstName;
+   }
+
+   public void setLastName(String lastName) {
+      this.lastName = lastName;
+   }
+
+   public void setEmail(String email) {
+      this.email = email;
+   }
+
+   @Override
+   public String toString() {
+      return "User{" +
+              "firstName='" + firstName + '\'' +
+              ", lastName='" + lastName + '\'' +
+              ", email='" + email + '\'' +
+              '}';
+   }
 }
